9c3a88f9f27d3a5e53182a197700048de6c1acb3
ouo_yoonk <joyk9251@gmail.com>
2021-03-27 09:43:23 +0900

    apply signup with firebase

--- sparta_react/image-community/src/elements/Input.js
+++ sparta_react/image-community/src/elements/Input.js
@@ -1,33 +1,54 @@
 import React from 'react';
 import styled from 'styled-components';
+
+import { Text, Grid } from './index';
+
 const Input = (props) => {
-  const { title, setValue, placeholder } = props;
+  const { label, placeholder, _onChange, type, multiLine } = props;
+
+  if (multiLine) {
+    return (
+      <Grid>
+        {label && <Text margin="0px">{label}</Text>}
+        <ElTextarea
+          rows={10}
+          placeholder={placeholder}
+          onChange={_onChange}
+        ></ElTextarea>
+      </Grid>
+    );
+  }
+
   return (
-    <InputContainer>
-      <span>{title}</span>
-      <input
-        type="text"
-        onChange={(e) => setValue(e.target.value)}
-        placeholder={placeholder}
-      />
-    </InputContainer>
+    <React.Fragment>
+      <Grid>
+        {label && <Text margin="0px">{label}</Text>}
+        <ElInput type={type} placeholder={placeholder} onChange={_onChange} />
+      </Grid>
+    </React.Fragment>
   );
 };
 
-const InputContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: start;
-  margin: 10px;
-
-  & > input {
-    height: 25px;
-  }
-`;
 Input.defaultProps = {
-  title: null,
-  setValue: null
+  multiLine: false,
+  label: false,
+  placeholder: '텍스트를 입력해주세요.',
+  type: 'text',
+  _onChange: () => {}
 };
+
+const ElTextarea = styled.textarea`
+  border: 1px solid #212121;
+  width: 100%;
+  padding: 12px 4px;
+  box-sizing: border-box;
+`;
+
+const ElInput = styled.input`
+  border: 1px solid #212121;
+  width: 100%;
+  padding: 12px 4px;
+  box-sizing: border-box;
+`;
 
 export default Input;
--- sparta_react/image-community/src/pages/Signup.js
+++ sparta_react/image-community/src/pages/Signup.js
@@ -1,71 +1,81 @@
-import React, { useState } from 'react';
-import { Text, Input, Button, Grid } from '../elements';
-import styled from 'styled-components';
+import React from 'react';
+import { Grid, Text, Input, Button } from '../elements';
+
 import { useDispatch } from 'react-redux';
 import { actionCreators as userActions } from '../redux/modules/user';
 
 const Signup = (props) => {
-  const [id, setId] = useState('');
-  const [nickname, setNickname] = useState('');
-  const [pw, setPw] = useState('');
-  const [pwChk, setChk] = useState('');
-
   const dispatch = useDispatch();
+
+  const [id, setId] = React.useState('');
+  const [pwd, setPwd] = React.useState('');
+  const [pwd_check, setPwdCheck] = React.useState('');
+  const [user_name, setUserName] = React.useState('');
+
   const signup = () => {
-    console.log(id, pw);
+    if (id === '' || pwd === '' || user_name === '') {
+      return;
+    }
+
+    if (pwd !== pwd_check) {
+      return;
+    }
+
+    dispatch(userActions.signupFB(id, pwd, user_name));
   };
   return (
-    <SignupContainer>
-      <Text bold="800" size="2em">
-        회원가입
-      </Text>
-      <Grid padding={'0px'}>
-        <Input
-          title={'아이디'}
-          placeholder={'아이디를 입력하세요'}
-          setValue={setId}
-        />
-      </Grid>
-      <Grid padding={'0px'}>
-        <Input
-          title={'닉네임'}
-          placeholder={'닉네임을 입력하세요'}
-          setValue={setPw}
-        />
-      </Grid>
-      <Grid padding={'0px'}>
-        <Input
-          title={'비밀번호'}
-          placeholder={'비밀번호를 입력하세요'}
-          setValue={setPw}
-        />
-      </Grid>
-      <Grid padding={'0px'}>
-        <Input
-          title={'비밀번호 확인'}
-          placeholder={'같은 비밀번호를 입력하세요'}
-          setValue={setPw}
-        />
-      </Grid>
-      <Grid padding={'0px'}>
-        <Button
-          disable={id && pw}
-          _onClick={signup}
-          type="full"
-          bgColor="black"
-          color="white"
-        >
+    <React.Fragment>
+      <Grid padding="16px">
+        <Text size="32px" bold>
           회원가입
-        </Button>
+        </Text>
+
+        <Grid padding="16px 0px">
+          <Input
+            label="아이디"
+            placeholder="아이디를 입력해주세요."
+            _onChange={(e) => {
+              setId(e.target.value);
+            }}
+          />
+        </Grid>
+
+        <Grid padding="16px 0px">
+          <Input
+            label="닉네임"
+            placeholder="닉네임을 입력해주세요."
+            _onChange={(e) => {
+              setUserName(e.target.value);
+            }}
+          />
+        </Grid>
+
+        <Grid padding="16px 0px">
+          <Input
+            label="비밀번호"
+            placeholder="비밀번호를 입력해주세요."
+            _onChange={(e) => {
+              setPwd(e.target.value);
+            }}
+          />
+        </Grid>
+
+        <Grid padding="16px 0px">
+          <Input
+            label="비밀번호 확인"
+            placeholder="비밀번호를 다시 입력해주세요."
+            _onChange={(e) => {
+              setPwdCheck(e.target.value);
+            }}
+          />
+        </Grid>
+
+        <Button text="회원가입하기" _onClick={signup}></Button>
       </Grid>
-    </SignupContainer>
+    </React.Fragment>
   );
 };
 
-const SignupContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
+Signup.defaultProps = {};
 
 export default Signup;
