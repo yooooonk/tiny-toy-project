ea848790517a640a60b1610c2699b0e5c8e14326
ouo_yoonk <joyk9251@gmail.com>
2021-07-15 06:40:54 +0900

    interface, alias

--- learn-typescript-master/quiz/1_todo/src/3_interface.ts
+++ learn-typescript-master/quiz/1_todo/src/3_interface.ts
@@ -13,3 +13,46 @@ function getUser(user:User){
 }
 
 getUser(saram1)
+
+interface SumFunction{
+    (a:number,b:number):number;
+}
+
+let sum: SumFunction;
+sum = function(a,b,){
+    return a+b
+}
+
+interface StringArray{
+    [index:number]:string;
+}
+
+const arr:StringArray = ['a','b','c']
+
+// indexing
+arr[0] = 'd'
+
+interface StringRegexDictionary{
+    [key:string]:RegExp // 정규표현식
+}
+
+const obj:StringRegexDictionary = {
+    sth:/abc/, // 통과
+    //sss:'abc', // 에러
+}
+
+
+interface Person{
+    name:string;
+    age:number;
+}
+
+interface Developer extends Person{    
+    language:string;
+}
+
+const me:Developer = {
+    name:'Gisele',
+    age:30,
+    language:'javascript'
+}
---
+++ learn-typescript-master/quiz/1_todo/src/4_alias.ts
@@ -0,0 +1,14 @@
+/* interface Person{
+    name:string;
+    age:number;
+} */
+
+type Personn = {
+    name:string;
+    age:number;
+}
+
+const seho:Personn={
+    name:'세호',
+    age:30
+}
