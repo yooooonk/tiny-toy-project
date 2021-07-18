bb901fd1aebde2df08af305ec30634dea657b78c
ouo_yoonk <joyk9251@gmail.com>
2021-07-19 06:45:46 +0900

    연산자를 이용한 타입 - 유니언타입, 인터섹션 타입

---
+++ learn-typescript-master/quiz/1_todo/src/5_operator.ts
@@ -0,0 +1,34 @@
+interface Pharmacy{
+    name:string;
+    hospital:string;
+}
+
+interface Designer{
+    name:string;
+    career:string;
+}
+
+function askSomeOne(someone:Pharmacy|Designer){
+    console.log(someone)}
+
+askSomeOne({name:'약사',hospital:'이비인후과'});
+askSomeOne({name:'디자이너',career:'골프웨어'});
+
+
+function askSomeBody(someone:Pharmacy&Designer){
+    console.log(someone)
+}
+askSomeBody({name:'헤이',hospital:'이비인후과'})
+askSomeBody({name:'헤이',hospital:'이비인후과',career:'골프웨어'})
+
+
+
+
+
+
+
+
+const name:string&number&boolean // 세 가지 모두를 만족할 수 없기 때문에 에러가 남
+
+
+
