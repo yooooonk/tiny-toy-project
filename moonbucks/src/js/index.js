7493eb2059ee98718008e4fc3a8ff5722f289a6a
ouo_yoonk <joyk9251@gmail.com>
2021-12-02 21:02:50 +0900

    [문벅스🌝][1. 메뉴판 만들기]  메뉴 추가 - 확인버튼 눌렀을 때 추가 기능 구현

--- moonbucks/src/js/index.js
+++ moonbucks/src/js/index.js
@@ -3,6 +3,41 @@ const $ = (selector) => document.querySelector(selector);
 
 const menuInput = $('#espresso-menu-name');
 const menuList = $('#espresso-menu-list');
+const submitButton = $('#espresso-menu-submit-button');
+
+const menuItemTemplate = (
+  menuItem
+) => `<li class="menu-list-item d-flex items-center py-2">
+                          <span class="w-100 pl-2 menu-name">${menuItem}</span>
+                          <button
+                              type="button"
+                              class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+                          >
+                              수정
+                          </button>
+                          <button
+                              type="button"
+                              class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+                          >
+                              삭제
+                          </button>
+                          </li>`;
+
+const resetInput = () => {
+  menuInput.value = '';
+};
+
+const updateCount = () => {
+  $('.menu-count').innerText = `총 ${menuList.childNodes.length}개`;
+};
+const addMenu = (menuItem) => {
+  if (menuItem.trim().length === 0) return alert('메뉴를 입력해주세요');
+
+  menuList.insertAdjacentHTML('beforeend', menuItemTemplate(menuItem));
+
+  resetInput();
+  updateCount();
+};
 
 function App() {
   $('#espresso-menu-form').addEventListener('submit', (e) =>
@@ -11,35 +46,12 @@ function App() {
 
   menuInput.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
-      // 메뉴 추가
-      const menuItemTemplate = (
-        menuItem
-      ) => `<li class="menu-list-item d-flex items-center py-2">
-                                <span class="w-100 pl-2 menu-name">${menuItem}</span>
-                                <button
-                                    type="button"
-                                    class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-                                >
-                                    수정
-                                </button>
-                                <button
-                                    type="button"
-                                    class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
-                                >
-                                    삭제
-                                </button>
-                                </li>`;
-
-      menuList.insertAdjacentHTML(
-        'beforeend',
-        menuItemTemplate(e.target.value)
-      );
-      // input reset
-      menuInput.value = '';
-
-      // count update
-      $('.menu-count').innerText = `총 ${menuList.childNodes.length}개`;
+      addMenu(e.target.value);
     }
+  });
+
+  submitButton.addEventListener('click', () => {
+    addMenu(menuInput.value);
   });
 }
 
