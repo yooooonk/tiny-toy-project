3609c8d221c470a474d8f072e2ea2ed811cabd10
ouo_yoonk <joyk9251@gmail.com>
2021-12-13 22:04:53 +0900

    [문벅스🌝][2. 여러가지 메뉴판 만들기]  메뉴 추가 - menu 배열 만들어서 추가할 때마다 렌더링

--- moonbucks/src/js/index.js
+++ moonbucks/src/js/index.js
@@ -1,60 +1,80 @@
 // 메뉴추가
 const $ = (selector) => document.querySelector(selector);
 
+const store = {
+  setLocalStorage(menu) {
+    localStorage.setItem('menu', JSON.stringify(menu));
+  },
+  getLocalStorage() {
+    localStorage.getItem('menu');
+  }
+};
+
 const menuInput = $('#espresso-menu-name');
 const menuList = $('#espresso-menu-list');
 const submitButton = $('#espresso-menu-submit-button');
 
-const menuItemTemplate = (
-  menuItem
-) => `<li class="menu-list-item d-flex items-center py-2">
-                          <span class="w-100 pl-2 menu-name">${menuItem}</span>
-                          <button
-                              type="button"
-                              class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-                          >
-                              수정
-                          </button>
-                          <button
-                              type="button"
-                              class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
-                          >
-                              삭제
-                          </button>
-                          </li>`;
-
-const resetInput = () => {
-  menuInput.value = '';
-};
-
-const updateCount = () => {
-  $('.menu-count').innerText = `총 ${menuList.childNodes.length}개`;
-};
-const addMenu = (menuItem) => {
-  if (menuItem.trim().length === 0) return alert('메뉴를 입력해주세요');
-
-  menuList.insertAdjacentHTML('beforeend', menuItemTemplate(menuItem));
-
-  resetInput();
-  updateCount();
-};
-
-const updateMenu = (currentMenu) => {
-  const editedMenu = window.prompt('메뉴를 수정하세요', currentMenu.innerText);
-  currentMenu.innerText = editedMenu;
-};
-
-const removeMenu = (currentMenu) => {
-  if (
-    window.confirm(
-      `${currentMenu.querySelector('.menu-name').innerText}를 삭제하겠습니까?`
-    )
-  ) {
-    currentMenu.remove();
-    updateCount();
-  }
-};
 function App() {
+  const menu = [];
+
+  const menuItemTemplate = (
+    menuItem,
+    id
+  ) => `<li data-menu-id=${id} class="menu-list-item d-flex items-center py-2">
+                            <span class="w-100 pl-2 menu-name">${menuItem}</span>
+                            <button
+                                type="button"
+                                class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+                            >
+                                수정
+                            </button>
+                            <button
+                                type="button"
+                                class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+                            >
+                                삭제
+                            </button>
+                            </li>`;
+
+  const resetInput = () => {
+    menuInput.value = '';
+  };
+
+  const updateCount = () => {
+    $('.menu-count').innerText = `총 ${menuList.childNodes.length}개`;
+  };
+
+  const updateMenu = (currentMenu) => {
+    const editedMenu = window.prompt(
+      '메뉴를 수정하세요',
+      currentMenu.innerText
+    );
+    currentMenu.innerText = editedMenu;
+  };
+
+  const removeMenu = (currentMenu) => {
+    if (
+      window.confirm(
+        `${currentMenu.querySelector('.menu-name').innerText}를 삭제하겠습니까?`
+      )
+    ) {
+      currentMenu.remove();
+      updateCount();
+    }
+  };
+  const addMenu = (menuItem) => {
+    if (menuItem.trim().length === 0) return alert('메뉴를 입력해주세요');
+
+    menu.push({ name: menuItem });
+    // 메뉴 배열을 렌더링
+    const lists = menu
+      .map((menu, idx) => menuItemTemplate(menu.name, idx))
+      .join('');
+    menuList.innerHTML = lists;
+
+    resetInput();
+    updateCount();
+  };
   $('#espresso-menu-form').addEventListener('submit', (e) =>
     e.preventDefault()
   );
